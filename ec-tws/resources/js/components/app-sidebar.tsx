7caccb417b834196e266765daffe3955496302bc
Oswald Mogot <oswaldmogot235@gmail.com>
2025-10-16 18:14:03 +0800

    nambahin ordernya mas

--- ec-tws/resources/js/components/app-sidebar.tsx
+++ ec-tws/resources/js/components/app-sidebar.tsx
@@ -4,7 +4,7 @@ import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
-import { BookOpen, Database, Grid2X2, Handshake, KeySquare, LayoutGrid, Package, ShoppingCart, Store, Users } from 'lucide-react';
+import { BookOpen, Database, Grid2X2, Handshake, KeySquare, LayoutGrid, NotebookPen, Package, ShoppingCart, Store, Users } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -43,6 +43,11 @@ const mainNavItems: NavItem[] = [
     href: route('transaction.index'),
     icon: Handshake,
   },
+  {
+    title: 'Orders',
+    href: route('order.index'),
+    icon: NotebookPen,
+  }
 ];
 
 const footerNavItems: NavItem[] = [];
